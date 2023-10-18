d2c1a753efb78c348d652cd00eeadb0a751a9398
chicchera <silvio.cavini@yahoo.it>
2023-10-18 17:37:57 -0300

    acquisition of text and main stats

--- obj.py
+++ obj.py
@@ -4,12 +4,14 @@
 
 import sys
 import re
+import regex
 import string
 import spacy
 import pyphen
 import timeit
 import time
 from rich import print
+from yaspin import yaspin
 from collections import namedtuple
 from utils.txt_utils import count_words, count_letters
 from language.dictionaries.dictionaries import Dictionaries
@@ -33,7 +35,7 @@ class TextProcessor:
         self._hyphen = None
         self._input_file = None
         self._requested_words = None
-        self._paragraphs_chunk = 500
+        self._paragraphs_chunk = 1000
         self._paragraphs = 0
         self._min_paragraph_length = float('inf')
         self._max_paragraph_length = 0
@@ -41,40 +43,88 @@ class TextProcessor:
         self._min_sentence_length = float('inf')
         self._max_sentence_length = 0
         self._words = 0
+        self._words_stats = False
         self._min_word_length = float('inf')
         self._max_word_length = 0
         self._letters = 0
         self._punctuation = 0
+        self._syllables = 0
+        self._syllabize = False
 
 
     def process_file(self, file_path, max_word_count):
+        def count_sentences(text):
+            doc = self._nlp(text)
+            return len(list(doc.sents))
+
+
+        def count_syllables(word):
+            return len(self._hyphen.inserted(word).split("-"))
+
+        # for word in words:
+        #     syllables_count += self.count_syllables(word)
+        #     letter_count += sum(1 for letter in word if letter.isalpha())
+
+
+        def count_words(text: str) -> tuple[int, int]:
+            """
+            Count the number of words in a given text.
+
+            Args:
+                text (str): The input text to count the words from.
+
+            Returns:
+                tuple[int, int]: The number of words in the input text and the number of syllables if `_syllabize` is True.
+            """
+            word_pattern = r'\w+'
+            words = re.findall(word_pattern, text, re.UNICODE)
+            syllables_count = 0
+            if self._syllabize:
+                syllables_count = sum(count_syllables(w) for w in words)
+
+            return len(words), syllables_count
+
+
         word_count = 0
+        syllables_count = 0
         chunk_count = 0
         chunk = ""
         num_paragraphs = 0
-        with open(file_path, 'r', encoding='utf-8') as file:
-            for line in file:
-                line = line.strip()
+        num_sentences = 0
+        num_letters = 0
 
-                if line:
-                    num_paragraphs += 1
-                    chunk += line + "\n\n"
-                    chunk_count += 1
-
-                    if chunk_count >= self._paragraphs_chunk:
-                        word_count += count_words(chunk)
-
+        print()
+        with yaspin().white.bold.shark.on_blue as spinner:
+            spinner.text = "Processing file..."
+            with open(file_path, 'r', encoding='utf-8') as file:
+                for line in file:
+                    if line:
+                        num_paragraphs += 1
+                        chunk += line + "\n\n"
+                        chunk_count += 1
+                        num_words, num_syllables = count_words(line)
+                        word_count += num_words
+                        syllables_count += num_syllables
+                        num_letters += count_letters(line)
                         if word_count >= max_word_count:
                             break
 
-                        chunk = ""
-                        chunk_count = 0
+                        if chunk_count >= self._paragraphs_chunk:
 
-            if chunk:
-                word_count += count_words(chunk)
+                            num_sentences += count_sentences(chunk)
 
-        self._words = word_count
-        self._paragraphs = num_paragraphs
+
+                            chunk = ""
+                            chunk_count = 0
+
+                if chunk.strip():
+                    num_sentences += count_sentences(chunk)
+
+            self._words = word_count
+            self._syllables = syllables_count
+            self._paragraphs = num_paragraphs
+            self._sentences = num_sentences
+            self._letters = num_letters
 
     @property
     def lang(self):
@@ -201,6 +251,31 @@ class TextProcessor:
         self._words = value
 
     @property
+    def syllables(self):
+        return self._syllables
+
+    @syllables.setter
+    def syllables(self, value):
+        self._syllables = value
+
+    @property
+    def syllabize(self):
+        return self._syllabize
+
+    @syllabize.setter
+    def syllabize(self, value):
+        self._syllabize = value
+
+
+    @property
+    def words_stats(self):
+        return self._words_stats
+
+    @words_stats.setter
+    def words_stats(self, value):
+        self._words_stats = value
+
+    @property
     def min_word_length(self):
         return self._min_word_length
 
@@ -236,12 +311,21 @@ tp = TextProcessor()
 tp.lang = "es"
 
 t = time.perf_counter()
+tp.syllabize = True
+tp.input_file = (JULIA, 200000)
 
-tp.input_file = (CATEDRAL, 500000)
+
 print()
 print(f"[dark_orange]{tp.requested_words:>{10},} [yellow1]requested words")
-print(f"[dark_orange]{tp.words:>{10},} [yellow1]counted words")
+
 print(f"[dark_orange]{tp.paragraphs:>{10},} [yellow1]paragraphs")
+print(f"[dark_orange]{tp.sentences:>{10},} [yellow1]sentences")
+
+print(f"[dark_orange]{tp.words:>{10},} [yellow1]counted words")
+print(f"[dark_orange]{tp.syllables:>{10},} [yellow1]syllables")
+
+print(f"[dark_orange]{tp.letters:>{10},} [yellow1]letters")
+
 print()
 elapsed_time = time.perf_counter() -t
 print(print(f"[yellow1]Elapsed time: [dark_orange]{elapsed_time:.2f} [yellow1]seconds"))
